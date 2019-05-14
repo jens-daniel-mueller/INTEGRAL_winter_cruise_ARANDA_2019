8ac8b5b74c1d879476898150143271829ff8687c
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-05-14 16:57:16 +0200

    included AKs flagged file

--- 1_Read_pCO2.R
+++ 1_Read_pCO2.R
@@ -47,8 +47,6 @@ HC.IOW %>%
   geom_path()
 
 
-
-
 # Read data from headspace equilibrator system ----------------------------
 # operated by IOW, Michael Glockzin
 # operated by GEOMAR, Annette Kock
@@ -75,6 +73,22 @@ Equi %>%
   geom_path()
 
 
+# Read data from headspace equilibrator system ----------------------------
+# post-processed and qc-ed by GEOMAR, Annette Kock
+
+Equi_corr <- read_delim(here::here("Data/Tracegases_Surface/Equi_IOW",
+                                 "ARANDA_equidata_orig_cleaned.csv"),
+                       na = "-999", delim = ";") %>% 
+  rename(date_time = X1) %>% 
+  filter(date_time > ymd_h("2019-02-28T11"))
+
+Equi_corr %>% 
+  filter(`pump source code equi(1=flowthrough, 2=ice well)` == 1) %>% 
+  ggplot(aes(date_time, CO2_ppm, col=as.factor(`baddata_equi (1==baddata)`)))+
+  geom_path(data = Equi, aes(date_time, CH4_CO2, col="raw"))+
+  geom_point()
+
+
 
 # subset relevant columns -------------------------------------------------
 
@@ -97,59 +111,65 @@ Equi <-
          "FlowSwEQ","FlowAirEQ","CH4_Press","CH4_Temp",
          "N2O_Press","N2O_Temp")
 
+Equi_corr <-
+  Equi_corr %>%
+  select("date_time","CH4_ppm","CO2_ppm","CO_ppm",
+         "N2O_ppm","TEqui", pump_equi = 17,
+         pump_sensors = 18, bad_data = 19)
+
 
 
 # Plots for quality control -----------------------------------------------
 
-
-
-library(plotly)
-
-ggplotly(
-ggplot()+
-  geom_path(data=HC.FMI, aes(date_time, pCO2_corr, col="HydroC_FMI"))+
-  geom_path(data=HC.IOW, aes(date_time, pCO2_corr, col="HydroC_IOW"))+
-  geom_path(data=Equi, aes(date_time, CH4_CO2, col="Equi_IOW"))+
-  labs(x="Date", y="pCO2 (?atm)")+
-  scale_color_brewer(palette = "Set1", name="Instrument")+
-  theme_bw()
-)
-
-
-ggplot()+
-  geom_path(data=HC.FMI, aes(date_time, pCO2_corr, col="HydroC_FMI"))+
-  geom_path(data=HC.IOW, aes(date_time, pCO2_corr, col="HydroC_IOW"))+
-  geom_path(data=Equi, aes(date_time, CH4_CO2, col="Equi_IOW"))+
-  labs(x="Date", y="pCO2 (?atm)")+
-  scale_color_brewer(palette = "Set1", name="Instrument")+
-  #ylim(400, 600)+
-  theme_bw()
-
-
-Equi %>% 
-ggplot()+
-  geom_point(aes(date_time, FlowSwEQ))+
-  theme_bw()
-
-Equi <- 
-  Equi %>% 
-  filter(FlowSwEQ >4)
-
-Equi %>% 
-ggplot()+
-  geom_point(aes(date_time, TInlet-TEqui))+
-  theme_bw()
-
-Equi <- 
-  Equi %>% 
-  filter(TInlet-TEqui > -1,
-         TInlet-TEqui < 0)
+# 
+# 
+# library(plotly)
+# 
+# ggplotly(
+# ggplot()+
+#   geom_path(data=HC.FMI, aes(date_time, pCO2_corr, col="HydroC_FMI"))+
+#   geom_path(data=HC.IOW, aes(date_time, pCO2_corr, col="HydroC_IOW"))+
+#   geom_path(data=Equi, aes(date_time, CH4_CO2, col="Equi_IOW"))+
+#   labs(x="Date", y="pCO2 (?atm)")+
+#   scale_color_brewer(palette = "Set1", name="Instrument")+
+#   theme_bw()
+# )
+# 
+# 
+# ggplot()+
+#   geom_path(data=HC.FMI, aes(date_time, pCO2_corr, col="HydroC_FMI"))+
+#   geom_path(data=HC.IOW, aes(date_time, pCO2_corr, col="HydroC_IOW"))+
+#   geom_path(data=Equi, aes(date_time, CH4_CO2, col="Equi_IOW"))+
+#   labs(x="Date", y="pCO2 (?atm)")+
+#   scale_color_brewer(palette = "Set1", name="Instrument")+
+#   #ylim(400, 600)+
+#   theme_bw()
+# 
+# 
+# Equi %>% 
+# ggplot()+
+#   geom_point(aes(date_time, FlowSwEQ))+
+#   theme_bw()
+# 
+# Equi <- 
+#   Equi %>% 
+#   filter(FlowSwEQ >4)
+# 
+# Equi %>% 
+# ggplot()+
+#   geom_point(aes(date_time, TInlet-TEqui))+
+#   theme_bw()
+# 
+# Equi <- 
+#   Equi %>% 
+#   filter(TInlet-TEqui > -1,
+#          TInlet-TEqui < 0)
 
 
 
 # Write summarized data files ---------------------------------------------
 
-write_csv(HC.IOW, here("Data/_summarized_data", "pCO2_HydroC_IOW.csv"))
-write_csv(HC.FMI, here("Data/_summarized_data", "pCO2_HydroC_FMI.csv"))
-write_csv(Equi, here("Data/_summarized_data", "pCO2_pCH4_Equilibrator_IOW.csv"))
+write_csv(HC.IOW, here::here("Data/_summarized_data", "pCO2_HydroC_IOW.csv"))
+write_csv(HC.FMI, here::here("Data/_summarized_data", "pCO2_HydroC_FMI.csv"))
+write_csv(Equi_corr, here::here("Data/_summarized_data", "pCO2_pCH4_Equilibrator_IOW.csv"))
 
